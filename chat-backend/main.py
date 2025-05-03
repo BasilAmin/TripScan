a50ce2fd469872a77f9c4420b80cf99946a4b9c2
oriol.grau.moragues <oriol.grau.moragues@estudiantat.upc.edu>
2025-05-03 10:37:43 +0200

    fastAPI

--- chat-backend/main.py
+++ chat-backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from messages import *
+from datetime import datetime
 
 app = FastAPI()
+
+messageID = 0
 
 class Message(BaseModel):
     message_id: str
@@ -17,7 +21,8 @@ async def get_messages():
 @app.post("/sendMessage/")
 async def send_message(message: Message):
     """Endpoint to send a message and save it to a CSV file."""
-    save_message_to_csv(message.message_id, message.user_id, message.content, message.timestamp)
+    save_message_to_csv(messageID, message.user_id, message.content, message.timestamp)
+    messageID += 1
     return {"status": "Message sent", "message_id": message.message_id}
 
 app.add_middleware(
