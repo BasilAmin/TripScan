5b95e2ba447edd238cfb6ab8793938193d2f20ec
David Borrego Valenzuela <davidborregomdl@gmail.com>
2025-05-03 14:00:35 +0200

    Fixed module errror.

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from pydantic import BaseModel
-from messages import *
+from .messages import *
 from datetime import datetime
 
 app = FastAPI()
