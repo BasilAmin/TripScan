c516493e5ffdd5ccd650f38a1a9e59cfc09e6a66
David Borrego Valenzuela <davidborregomdl@gmail.com>
2025-05-03 13:24:41 +0200

    Change vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,12 +11,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: path.resolve(__dirname, 'frontend/build'), // change output directory here
+  },
 }));
+
